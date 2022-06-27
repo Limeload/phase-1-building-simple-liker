b94c59d7de432586245c386475cf70c50758b8ce
Shraddha Rao <shraddharaom@gmail.com>
2022-06-27 19:22:09 +0530

    commit tree 650fadedc91eabc44409c3359134dcaec52cb956

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
+const { getLikes } = require("unsplash-js/dist/methods/users")
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+const modal = document.getElementById("modal")
+modal.setAttribute("class", "hidden") 
+
+function likeCallback() {
+  document.addEventListener('click', (event) => {
+  if (event.target.classList[0] === 'like-glyph') {
+    mimicServerCall()
+    .then(resp => {
+      const activated = event.target.classList.contains("actiavted-heart");
+      if (activated) {
+      event.target.classList.remove("activated-heart");
+      event.target.innerHTML = EMPTY_HEART;
+      } else {
+        event.target.classList.add("activated-heart");
+        event.target.innerHTML = FULL_HEART;
+      }
+
+      activated;
+    })
+    .catch(error => {
+      // modal.hidden = false
+      modal.classList.remove("hidden");
+      const modalMessage = document.querySelector("#modal-message")
+      modalMessage.innerText = error
+      setTimeout(() => {
+        modal.hidden = true
+      }, 5000)
+    })
+}});
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
